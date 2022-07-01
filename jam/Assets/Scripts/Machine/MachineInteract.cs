7a23b3ab5b538364af6a6016fa3bbeed092d4cf7
TermiSenpai <escarpaprieto@gmail.com>
2022-07-01 12:19:23 +0200

    Player stop moving on interact

--- jam/Assets/Scripts/Machine/MachineInteract.cs
+++ jam/Assets/Scripts/Machine/MachineInteract.cs
@@ -1,30 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MachineInteract : MonoBehaviour
 {
-    [SerializeField] public GameObject text;
+    [SerializeField] private GameObject text;
+    [SerializeField] private bool isTouchingMachine;
+    [SerializeField] private PlayerMove player;
 
-    // Start is called before the first frame update
     void Start()
     {
         text.SetActive(false);
+        isTouchingMachine = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         text.SetActive(true);
+        isTouchingMachine = true;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         text.SetActive(false);
+        isTouchingMachine = false;
+    }
+
+    public void Interact(InputAction.CallbackContext callback)
+    {
+        if (isTouchingMachine && callback.phase == InputActionPhase.Started)
+        {
+            player.canMove = false;
+        }
+    }
+
+    // TEMPORAL
+    public void Pause(InputAction.CallbackContext callback)
+    {
+        if (isTouchingMachine && callback.phase == InputActionPhase.Started)
+            player.canMove = true;
     }
 }
