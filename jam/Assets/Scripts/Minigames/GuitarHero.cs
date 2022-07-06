a441072d36ce7ac792b725c7eecabe0f30c81853
Angel M <amagan17@gmail.com>
2022-07-06 18:59:03 +0200

    Parte del guitar hero
    No se si te servira, sorry por no tener tiempo

--- jam/Assets/Scripts/Minigames/GuitarHero.cs
+++ jam/Assets/Scripts/Minigames/GuitarHero.cs
@@ -5,33 +5,73 @@ using UnityEngine.InputSystem;
 
 public class GuitarHero : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer[] botones;
-    public InputAction playerController;
+    [SerializeField] private ButtonInfo[] botones;
 
-    private void OnEnable()
+    public void ButtonUp(InputAction.CallbackContext context)
     {
-        playerController.Enable();
-    }
-    private void OnDisable()
-    {
-        playerController.Disable();
-    }
-    void Start()
-    {
-        
-    }
-
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (context.phase == InputActionPhase.Started)
         {
             PressButton(0);
         }
+        if (context.phase == InputActionPhase.Canceled)
+        {
+            ReleaseButton(0);
+        }
+    }
+    public void ButtonDown(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
+        {
+            PressButton(1);
+        }
+        if (context.phase == InputActionPhase.Canceled)
+        {
+            ReleaseButton(1);
+        }
+    }
+    public void ButtonRight(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
+        {
+            PressButton(2);
+        }
+        if (context.phase == InputActionPhase.Canceled)
+        {
+            ReleaseButton(2);
+        }
+    }
+    public void ButtonLeft(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
+        {
+            PressButton(3);
+        }
+        if (context.phase == InputActionPhase.Canceled)
+        {
+            ReleaseButton(3);
+        }
     }
 
-    private void PressButton(int button)
+    private void PressButton (int button)
     {
-        botones[button].color = Color.red;
+        ButtonInfo buttonInfo = botones[button];
+        if (buttonInfo == null) return;
+
+        buttonInfo.buttonSpriteRenderer.color = buttonInfo.darkColor;
+    }
+    private void ReleaseButton(int button)
+    {
+        ButtonInfo buttonInfo = botones[button];
+        if (buttonInfo == null) return;
+
+        buttonInfo.buttonSpriteRenderer.color = buttonInfo.originalColor;
+    }
+    private void SpawnNotes(int button)
+    {
+        ButtonInfo buttonInfo = botones[button];
+        if (buttonInfo == null) return;
+
+
+
     }
 }
