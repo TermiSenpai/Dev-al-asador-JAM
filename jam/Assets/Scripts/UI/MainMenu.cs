3f4dac6d801f3bb194b9fc89f89b494cb378568b
TermiSenpai <escarpaprieto@gmail.com>
2022-07-08 04:48:29 +0200

    MainMenu canvaUI (no funcionality)

---
+++ jam/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+    private void Update()
+    {
+    }
+
+    public void startGame()
+    {
+
+    }
+
+    public void openOptions()
+    {
+
+    }
+
+    public void exitGame()
+    {
+
+    }
+}
